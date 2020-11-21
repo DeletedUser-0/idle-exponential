b5829b0d1d309996c90bb43fca0ce562ef2b9f4d
DeletedUser-0 <71786022+DeletedUser-0@users.noreply.github.com>
2020-11-21 12:38:31 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,58 +1,69 @@
+// Main game values.
 var number = new Decimal(1);
 var multiplier1 = new Decimal(1);
 var TotalAmount = new Decimal(1);
 var multiplier2 = new Decimal(0.00025);
-var multiplier3 = new Decimal(0.99999995);
-var IP = new Decimal(2);
+var multiplier3 = new Decimal(1);
+var IP = new Decimal(0);
+var OldIP = new Decimal(0);
+var multiplierEffect1 = new Decimal(1);
 
-
+// Multiplies the number every tick.
 function multiply() {
     number = number.times(multiplier1);
     TotalAmount = TotalAmount.times(multiplier1);
     document.getElementById('number').innerHTML = "Number: " + notate(number);
     document.getElementById('TotalNumber').innerHTML = "Total Received: " + notate(TotalAmount);
-    document.getElementById('multiplier').innerHTML = "Multiplier per second: " + notate2((Decimal.pow(multiplier1, 10))) + "x";
+    document.getElementById('multiplier').innerHTML = "Multiplier per second: " + notate2((Decimal.pow(multiplier1, 25))) + "x";
 }
 
-multiplier2 = (multiplier2.add(1))
+// This had to be added or the game would be unbalanced.
+multiplier2 = (multiplier2.add(1));
 
+// Multiplies the multiplier1 value, which that variable multiplies the main number value.
 function upgrade1() {
     multiplier1 = multiplier1.times(multiplier2);
 	multiplier2 = multiplier2.times(multiplier3);
-	IP = Math.floor(number.log(1e90));
     document.getElementById('number').innerHTML = `Number: ${notate(number)}`;
-    document.getElementById('multiplier').innerHTML = "Multiplier per second: " + notate2((Decimal.pow(multiplier1, 10))) + "x";
-	document.getElementById('IP').innerHTML = "Prestige to get " + (IP) + " IP";
+    document.getElementById('multiplier').innerHTML = "Multiplier per second: " + notate2((Decimal.pow(multiplier1, 25))) + "x";
+	document.getElementById('IPamount').innerHTML = "You have " + OldIP + " Infinity Points";
+	document.getElementById('IP').innerHTML = "Earn " + Math.floor(number.log(1e25)) + " Infinity Points";
 }
 
+// Click to earn more Infinity Points.
 function EarnIP() {
-		number = new Decimal(1);
-		multiplier1 = new Decimal(1);
-		multiplier2 = (new Decimal(0.00025).times(IP).add(1));
-		multiplier3 = new Decimal(0.99999995).sub(0.0000005);
-		document.getElementById('IPamount').innerHTML = "You have " + IP + " IP"
+	IP = Math.floor(number.log(1e25));
+	OldIP = OldIP.add(IP);
+	multiplierEffect1 = OldIP.log(1.25);
+	number = new Decimal(1);
+	multiplier1 = new Decimal(1);
+	multiplier2 = (new Decimal(0.00025).times(multiplierEffect1).add(1);
+	multiplier3 = new Decimal(1).sub(0.0000001);
+	document.getElementById('IPamount').innerHTML = "You have " + OldIP.sub(2) + " Infinity Points"
+	document.getElementById('IP').innerHTML = "Prestige and you'll have " + Math.floor(number.log(1e25)) + " Infinity Points";
 }
-
 	
-	
+// This notates both values in HTML.	
 function notate(n) {
     var m = n.mantissa;
     var e = n.exponent;
-    if (e < 3) return (m * Math.pow(10, e)).toPrecision(3)
+    if (e < 3) return (m * Math.pow(10, e)).toPrecision(3);
     return `${m.toPrecision(3)}e${e}`;
 }
 
 function notate2(n) {
     var m = n.mantissa;
     var e = n.exponent;
-    if (e < 3) return (m * Math.pow(10, e)).toPrecision(3)
+    if (e < 3) return (m * Math.pow(10, e)).toPrecision(3);
     return `${m.toPrecision(2)}e${e}`;
 }
 
+// Automation at the start of the game!
+
 var mainGameLoop = window.setInterval(function () {
     multiply()
-}, 100)
+}, 50);
 
 var mainGameLoop = window.setInterval(function () {
     upgrade1()
-}, 25)
+}, 25);
