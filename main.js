be91e1e493a183656f57e811ff65c8734ed7dce6
DeletedUser-0 <71786022+DeletedUser-0@users.noreply.github.com>
2020-11-22 12:44:58 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 // Main game values.
 var number = new Decimal(1);
 var multiplier1 = new Decimal(1);
-var TotalAmount = new Decimal(1);
 var multiplier2 = new Decimal(0.00000000000001);
 var IP = new Decimal(1);
 var OldIP = new Decimal(1);
 var MultiplierEffect1 = new Decimal(1);
+var Upgrade1Cost = new Decimal(1e6);
+var Upgrade1Level = new Decimal(0);
+var BaseEffect1 = new Decimal(0.0125);
 
 // Multiplies the number every tick.
 function multiply() {
     number = number.times(multiplier1);
-    TotalAmount = TotalAmount.times(multiplier1);
     document.getElementById('number').innerHTML = "Number: " + notate(number);
-    document.getElementById('TotalNumber').innerHTML = "Total Received: " + notate(TotalAmount);
-    document.getElementById('multiplier').innerHTML = "(x" + notate3((Decimal.pow(multiplier1, 25))) + "/s)";
+    document.getElementById('multiplier').innerHTML = "(x" + notate3((Decimal.pow(multiplier1, 20))) + "/s)";
 }
 
 multiplier1 = multiplier1.add(1);
@@ -22,7 +22,7 @@ multiplier1 = multiplier1.add(1);
 function upgrade1() {
     multiplier1 = MultiplierEffect1.divide(1.45).times(multiplier1.log(2)).add(1.0000001);
     document.getElementById('number').innerHTML = `Number: ${notate(number)}`;
-    document.getElementById('multiplier').innerHTML = "(x" + notate3((Decimal.pow(multiplier1, 25))) + "/s)";
+    document.getElementById('multiplier').innerHTML = "(x" + notate3((Decimal.pow(multiplier1, 20))) + "/s)";
 	document.getElementById('IPamount').innerHTML = "You have " + notate(OldIP) + " Infinity Points";
 }
 
@@ -30,10 +30,21 @@ function upgrade1() {
 function EarnIP() {
 	IP = IP.times(1.003);
 	OldIP = IP;
-	MultiplierEffect1 = OldIP.pow(0.0125);
+	MultiplierEffect1 = OldIP.pow(BaseEffect1);
 	multiplier2 = (new Decimal(0.00002).times(MultiplierEffect1));
 	document.getElementById('IPamount').innerHTML = "You have " + notate(OldIP) + " Infinity Points";
 }
+
+function IncreaseMultiplier1() {
+	if (Upgrade1Cost >= Number) {
+		BaseEffect1.times(1.04);
+		Upgrade1Cost.pow(1.35);
+		Upgrade1Level.add(1);
+		document.getElementById('number').innerHTML = "Number: " + notate(number);
+		document.getElementById('multiplier').innerHTML = "(x" + notate3((Decimal.pow(multiplier1, 20))) + "/s)";
+		document.getElementById('Upgrade1').innerHTML = "Increase Multiplier per second. <br> Cost: " + notate(Upgrade1Cost) + "<br> Level: " + notate4(Upgrade1Level); 
+	};
+};
 	
 // This notates both values in HTML.	
 function notate(n) {
@@ -57,6 +68,13 @@ function notate3(n) {
     return `${m.toPrecision(3)}e${e.toLocaleString("pt-BR")}`;
 }
 
+function notate4(n) {
+    var m = n.mantissa;
+    var e = n.exponent;
+    if (e < 3) return (m * Math.pow(10, e.toLocaleString("pt-BR"))).toPrecision(1);
+    return `${m.toPrecision(3)}e${e.toLocaleString("pt-BR")}`;
+}
+		
 // Automation at the start of the game!
 
 var mainGameLoop = window.setInterval(function () {
@@ -70,3 +88,24 @@ var mainGameLoop = window.setInterval(function () {
 var mainGameLoop = window.setInterval(function () {
     EarnIP()
 }, 500);
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
