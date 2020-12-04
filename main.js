d138686bf4fc48540d505aa85252244e5a826db5
DeletedUser-0 <71786022+DeletedUser-0@users.noreply.github.com>
2020-12-04 23:22:53 +0000

    Update main.js

--- main.js
+++ main.js
@@ -127,19 +127,3 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
-
-// Retrieve your data from locaStorage
-var saveData = JSON.parse(localStorage.saveData || null) || {};
-
-// Store your data.
-function saveStuff(obj) {
-  saveData.obj = obj;
-  // saveData.foo = foo;
-  saveData.time = new Date().getTime();
-  localStorage.saveData = JSON.stringify(saveData);
-}
-
-// Do something with your data.
-function loadStuff() {
-  return saveData.obj || "default";
-}
